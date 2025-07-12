b414f9e8ab124140ed0b021d7b115784e1dd154f
Devesh Chandra <deveshru2712@gmail.com>
2025-07-12 20:02:03 +0530

    add themes to the project and fix navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,7 +44,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,27 @@
+"use client";
 import { Search } from "lucide-react";
 import Link from "next/link";
+import { Toggle } from "./Toggle";
 
 export default function Navbar() {
   return (
-    <div className="w-full py-4 px-4 sm:px-6 lg:px-8 shadow-lg">
+    <div className="w-full py-4 px-4 sm:px-6 lg:px-8">
       <nav className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <Link
             href="/"
-            className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 hover:scale-105 transition-transform duration-300"
+            className="text-2xl font-bold text-black/90 dark:text-slate-200"
           >
             Whispers
           </Link>
 
           {/* Navigation Links */}
           <div className="hidden md:flex items-center justify-center flex-1">
-            <ul className="text-slate-200 text-base font-semibold flex items-center gap-12">
+            <ul className="text-black/90 dark:text-slate-400 text-base font-semibold flex items-center gap-12">
               <li>
                 <Link
                   href="#"
-                  className="relative group hover:text-white transition-colors duration-300"
+                  className="relative group hover:text-slate-500 dark:hover:text-slate-100 transition-colors duration-300"
                 >
                   Home
                 </Link>
@@ -27,7 +29,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="relative group hover:text-white transition-colors duration-300"
+                  className="relative group hover:text-slate-500 dark:hover:text-slate-100 transition-colors duration-300"
                 >
                   About
                 </Link>
@@ -35,7 +37,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="relative group hover:text-white transition-colors duration-300"
+                  className="relative group hover:text-slate-500 dark:hover:text-slate-100 transition-colors duration-300"
                 >
                   Contact
                 </Link>
@@ -43,16 +45,20 @@ export default function Navbar() {
             </ul>
           </div>
 
-          <div className="flex items-center gap-1.5 border border-slate-50/20 px-2 py-1 rounded-xl group  transition-all duration-300 focus-within:border-slate-100/60 focus-within:ring-1 focus-within:ring-slate-100/30">
-            <Search
-              size={18}
-              className="text-slate-300 group-focus-within:text-white transition-colors duration-300"
-            />
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-transparent focus:outline-none text-slate-200 placeholder-slate-400 w-full"
-            />
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1.5 border border-slate-300 dark:border-slate-500/30 px-2 py-1 rounded-xl group transition-all duration-300 focus-within:border-slate-700 focus-within:ring-1 focus-within:ring-slate-70030 dark:focus-within:border-slate-200 dark:focus-within:ring-slate-800/30">
+              <Search
+                size={22}
+                className="text-slate-950 dark:text-white font-semibold transition-colors duration-300"
+              />
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent focus:outline-none text-slate-800 dark:text-slate-200 placeholder-slate-400 w-full"
+              />
+            </div>
+
+            <Toggle />
           </div>
         </div>
       </nav>
